98c21d373b41f0a24b62555746f616bccad29646
leonelp <pereiraleonel287@gmail.com>
2024-09-22 20:17:32 -0500

    1er commit agregando main

---
+++ src/main/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ASUS TUF
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("POOGame");
+        
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+    
+}
